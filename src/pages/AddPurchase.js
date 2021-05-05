9062e8a6eeb590c9c1a8e92184a6e6ee4101fd68
snipextt <snipextt@gmail.com>
2021-05-06 02:23:20 +0530

    Added Routes to add purchase

--- src/pages/AddPurchase.js
+++ src/pages/AddPurchase.js
@@ -1,5 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { Input, Label, Button, Select, Textarea } from '@windmill/react-ui';
+import {
+  Input,
+  Label,
+  Button,
+  Select,
+  Textarea,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from '@windmill/react-ui';
 import PageTitle from '../components/Typography/PageTitle';
 import { MailIcon, PhoneIcon, UserIcon, ProductIcon } from '../icons';
 import SectionTitle from '../components/Typography/SectionTitle';
@@ -7,12 +17,31 @@ import axios from 'axios';
 
 function AddPurchase() {
   const [categories, setCategories] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [productPrice, setProductPrice] = useState(0);
+  const [productQuantity, setProductQuantity] = useState(1);
+  const [user, setUser] = useState(null);
+  const [modalStatus, setModalStatus] = useState({});
+
   useEffect(() => {
     (async () => {
       const categories = await axios.get('/products/categories');
-      setCategories(categories);
+      console.log(categories);
+      setCategories(categories.data);
+      setSelectedCategory(categories.data[0]._id);
     })();
   }, []);
+  useEffect(() => {
+    (async () => {
+      const products = await axios
+        .get(`/products/${selectedCategory}`)
+        .catch((err) => console.log(err.response));
+      setProducts(products.data);
+      setProductPrice(products.data[0].price);
+    })();
+  }, [selectedCategory]);
   return (
     <>
       <PageTitle>Add purchase</PageTitle>
@@ -27,9 +56,16 @@ function AddPurchase() {
               Category
             </label>
             <div className="relative">
-              <Select>
-                <option>Shoes</option>
-                <option>Bags</option>
+              <Select
+                id="category"
+                onChange={(e) => setSelectedCategory(e.target.value)}
+              >
+                {!categories.length && <option>Loading</option>}
+                {categories.map((category) => (
+                  <option key={category._id} value={category._id}>
+                    {category.name}
+                  </option>
+                ))}
               </Select>
             </div>
           </div>
@@ -37,16 +73,33 @@ function AddPurchase() {
             <label className="block tracking-wide text-grey-darker text-sm mb-2">
               Product
             </label>
-            <Select>
-              <option>Nike</option>
-              <option>Nike</option>
+            <Select
+              id="product"
+              onChange={(e) =>
+                setProductPrice(
+                  products.find((p) => p._id == e.target.value).price
+                )
+              }
+            >
+              {!products.length && <option>Loading</option>}
+              {products.map((product) => (
+                <option key={product._id} value={product._id}>
+                  {product.name}
+                </option>
+              ))}
             </Select>
           </div>
           <div className="md:w-1/2 px-3">
             <label className="block tracking-wide text-grey-darker text-sm mb-2">
-              Purchase Time
+              Quantity
             </label>
-            <Input disabled className="mt-1" value={`${new Date()} `} />
+            <Input
+              id="quantity"
+              type="number"
+              className="mt-1"
+              defaultValue={1}
+              onChange={(e) => setProductQuantity(e.target.value)}
+            />
           </div>
         </div>
         <div className="-mx-3 md:flex mb-6">
@@ -56,8 +109,11 @@ function AddPurchase() {
             </label>
             <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400">
               <input
+                readOnly
+                id="price"
                 className="block w-full pl-10 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
-                placeholder="user@wms.com"
+                placeholder="₹"
+                value={productPrice * productQuantity}
               />
               <div className="absolute inset-y-0 flex items-center ml-3 pointer-events-none">
                 <ProductIcon className="w-6 h-6" aria-hidden="true" />
@@ -70,8 +126,10 @@ function AddPurchase() {
             </label>
             <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400">
               <input
+                id="tax"
                 className="block w-full pl-10 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
-                placeholder="user@wms.com"
+                placeholder="₹"
+                defaultValue={0}
               />
               <div className="absolute inset-y-0 flex items-center ml-3 pointer-events-none">
                 <ProductIcon className="w-6 h-6" aria-hidden="true" />
@@ -85,6 +143,7 @@ function AddPurchase() {
               Description
             </label>
             <Textarea
+              id="description"
               className="mt-1"
               rows="3"
               placeholder="An Optional Description."
@@ -99,7 +158,14 @@ function AddPurchase() {
               <span>Email</span>
               <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400">
                 <input
+                  id="email"
                   type="email"
+                  onBlur={async (e) => {
+                    const user = await axios
+                      .get(`/users/${e.target.value}`)
+                      .catch((err) => console.error(err));
+                    if (user) setUser(user);
+                  }}
                   className="block w-full pl-10 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
                   placeholder="user@wms.com"
                 />
@@ -113,6 +179,7 @@ function AddPurchase() {
             <span>Name</span>
             <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400">
               <input
+                id="name"
                 className="block w-full pl-10 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
                 placeholder="Jhon Doe"
               />
@@ -129,8 +196,9 @@ function AddPurchase() {
             <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400">
               <input
                 type="tel"
+                id="phone"
                 className="block w-full pl-10 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
-                placeholder="Jhon Doe"
+                placeholder="900909090"
               />
               <div className="absolute inset-y-0 flex items-center ml-3 pointer-events-none">
                 <PhoneIcon className="w-6 h-6" aria-hidden="true" />
@@ -139,9 +207,66 @@ function AddPurchase() {
           </Label>
         </div>
         <div className="px-6 my-6">
-          <Button>Add Purchase</Button>
+          <Button
+            onClick={async () => {
+              let userId = user ? user._id : undefined;
+              if (!user) {
+                let password = Math.random().toString(36).substring(2, 12);
+                const user = {
+                  name: document.querySelector('#name').value,
+                  password,
+                  email: document.querySelector('#email').value,
+                  phone: document.querySelector('#phone').value,
+                  accountType: 'c',
+                };
+                const newUser = await axios
+                  .post('/users', user)
+                  .catch((err) => console.error(err.response));
+                userId = newUser.data._id;
+              }
+
+              const Purchase = {
+                category: document.querySelector('#category').value,
+                product: document.querySelector('#product').value,
+                quantity: document.querySelector('#price').value,
+                tax: document.querySelector('#tax').value,
+                price: document.querySelector('#price').value,
+                description: document.querySelector('#description').value,
+                user: userId,
+              };
+              const newPurchase = await axios
+                .post('/shop', Purchase)
+                .catch((err) => console.error(err));
+              if (newPurchase) {
+                setModalStatus({
+                  header: 'Purchase added',
+                  body: 'Purchase added with details specified',
+                });
+              } else {
+                setModalStatus({
+                  header: 'Purchase failed',
+                  body: 'Check the entry fields',
+                });
+              }
+              setIsModalOpen(true);
+            }}
+          >
+            Add Purchase
+          </Button>
         </div>
       </div>
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        <ModalHeader>{modalStatus.header}</ModalHeader>
+        <ModalBody>{modalStatus.body}</ModalBody>
+        <ModalFooter>
+          <Button
+            onClick={() => setIsModalOpen(false)}
+            className="w-full sm:w-auto"
+          >
+            Proceed
+          </Button>
+        </ModalFooter>
+      </Modal>
     </>
   );
 }
