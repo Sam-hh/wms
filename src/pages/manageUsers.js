445cd6ccb1aaec04c6d96e6a4ffe385768f50270
snipextt <snipextt@gmail.com>
2021-05-22 14:01:11 +0530

    Added updateuser options

--- src/pages/manageUsers.js
+++ src/pages/manageUsers.js
@@ -17,9 +17,17 @@ import {
   ModalBody,
   ModalFooter,
   Input,
+  Label,
 } from '@windmill/react-ui';
-import response from '../utils/demo/tableData';
-import { resetIcon, SearchIcon } from '../icons';
+import {
+  penIcon,
+  resetIcon,
+  SearchIcon,
+  UserIcon,
+  MailIcon,
+  PhoneIcon,
+  BinIcon,
+} from '../icons';
 import axios from 'axios';
 
 function manageUsers() {
@@ -31,6 +39,8 @@ function manageUsers() {
   const resultsPerPage = 10;
   const [totalResults, setTotalResults] = useState(20);
   const [modalStatus, setModalStatus] = useState({});
+  const [userUpdateModal, setuserUpdateModal] = useState(false);
+  const [userToUpdate, setUserToUpdate] = useState({});
 
   // pagination change control
   function onPageChange(p) {
@@ -116,10 +126,23 @@ function manageUsers() {
                   <span className="text-sm">
                     <span className="has-tooltip">
                       <span className="tooltip rounded shadow-lg p-1 bg-gray-500 text-white -mt-8 -ml-6">
+                        Edit User Deatils
+                      </span>
+                      <Button
+                        icon={penIcon}
+                        layout="link"
+                        aria-label="Link"
+                        onClick={async () => {
+                          setuserUpdateModal(true);
+                          setUserToUpdate(user);
+                        }}
+                      />
+                    </span>
+                    <span className="has-tooltip">
+                      <span className="tooltip rounded shadow-lg p-1 bg-gray-500 text-white -mt-8 -ml-6">
                         Reset Password
                       </span>
                       <Button
-                        className="ml-4"
                         icon={resetIcon}
                         layout="link"
                         aria-label="Link"
@@ -176,6 +199,161 @@ function manageUsers() {
             className="w-full sm:w-auto"
           >
             Proceed
+          </Button>
+        </ModalFooter>
+      </Modal>
+      <Modal isOpen={userUpdateModal} onClose={() => setuserUpdateModal(false)}>
+        <ModalHeader>Update User</ModalHeader>
+        <ModalBody>
+          <div className="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
+            <div className="mt-4">
+              <Label>
+                <span>Name</span>
+                <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400">
+                  <input
+                    className="block w-full pl-10 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+                    defaultValue={userToUpdate.name}
+                    id="name"
+                  />
+                  <div className="absolute inset-y-0 flex items-center ml-3 pointer-events-none">
+                    <UserIcon className="w-5 h-5" aria-hidden="true" />
+                  </div>
+                </div>
+              </Label>
+            </div>
+
+            <div className="mt-4">
+              <Label>
+                <span>Email</span>
+                <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400">
+                  <input
+                    type="email"
+                    className="block w-full pl-10 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+                    defaultValue={userToUpdate.email}
+                    id="email"
+                  />
+                  <div className="absolute inset-y-0 flex items-center ml-3 pointer-events-none">
+                    <MailIcon className="w-5 h-5" aria-hidden="true" />
+                  </div>
+                </div>
+              </Label>
+            </div>
+
+            <div className="mt-4">
+              <Label>
+                <span>Phone</span>
+                <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400">
+                  <input
+                    type="tel"
+                    className="block w-full pl-10 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+                    defaultValue={userToUpdate.phone}
+                    id="phone"
+                  />
+                  <div className="absolute inset-y-0 flex items-center ml-3 pointer-events-none">
+                    <PhoneIcon className="w-6 h-6" aria-hidden="true" />
+                  </div>
+                </div>
+              </Label>
+            </div>
+
+            <div className="mt-4">
+              <Label>Account Type</Label>
+              <div className="mt-2">
+                <Label radio>
+                  <Input
+                    defaultChecked={userToUpdate.accountType == 'e'}
+                    type="radio"
+                    value="e"
+                    name="accountType"
+                  />
+                  <span className="ml-2">Employee</span>
+                </Label>
+                <Label className="ml-6" radio>
+                  <Input
+                    type="radio"
+                    defaultChecked={userToUpdate.accountType == 'c'}
+                    value="c"
+                    name="accountType"
+                  />
+                  <span className="ml-2">Customer</span>
+                </Label>
+              </div>
+            </div>
+          </div>
+        </ModalBody>
+        <ModalFooter
+          style={{
+            diaply: 'flex',
+            justifyContent: 'space-between',
+          }}
+        >
+          <Button
+            style={{ fill: 'white' }}
+            onClick={async () => {
+              const res = await axios
+                .delete(`/users/${userToUpdate._id}`)
+                .catch(console.error);
+              if (res.status < 400) {
+                setuserUpdateModal(false);
+                data.splice(
+                  data.findIndex((c) => c._id == userToUpdate._id),
+                  1,
+                  response[
+                    response.findIndex((c) => c._id == userToUpdate._id) + 1
+                  ]
+                );
+                response.splice(
+                  response.findIndex((c) => c._id == userToUpdate._id),
+                  1
+                );
+                setTotalResults((prevCount) => prevCount - 1);
+              }
+            }}
+            iconLeft={BinIcon}
+          >
+            Remove
+          </Button>
+          <Button
+            onClick={async () => {
+              const user = {
+                name: document.querySelector('#name').value,
+                email: document.querySelector('#email').value,
+                phone: document.querySelector('#phone').value,
+                accountType: document.querySelector(
+                  "input[name='accountType']:checked"
+                ).value,
+              };
+              const res = await axios
+                .patch(`/users/update/${userToUpdate._id}`, user)
+                .catch(console.error);
+              if (res.status < 400) {
+                setModalStatus({
+                  header: 'User Changed',
+                  body: 'Updated the specified user fields',
+                });
+                let matchedDataIndex = data.findIndex(
+                  (u) => u._id == userToUpdate._id
+                );
+                let matchedResponsesIndex = response.findIndex(
+                  (u) => u._id == userToUpdate._id
+                );
+                response[matchedResponsesIndex] = {
+                  ...response[matchedResponsesIndex],
+                  ...user,
+                };
+                data[matchedDataIndex] = { ...data[matchedDataIndex], ...user };
+              } else {
+                setModalStatus({
+                  header: 'User Change Failed',
+                  body: 'Sonething went wrong, please try later!',
+                });
+              }
+              setIsModalOpen(true);
+              setuserUpdateModal(false);
+            }}
+            className="w-full sm:w-auto"
+          >
+            Update
           </Button>
         </ModalFooter>
       </Modal>
